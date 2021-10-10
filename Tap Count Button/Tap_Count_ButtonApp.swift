0af662d792ac1c67d68236050d2eb50eff4e2fc7
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-10-09 21:34:12 -0400

    Initial Commit

---
+++ Tap Count Button/Tap_Count_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_Count_ButtonApp.swift
+//  Tap Count Button
+//
+//  Created by Franco Rodrigues on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Tap_Count_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
